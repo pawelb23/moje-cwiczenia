0f6da219d814c1c47b0418515395b188e3f54c40
Paweł Białek <pawelbialek23@op.pl>
2018-02-23 11:00:16 +0100

    kalkulator

--- kalkulator-przyklad/js/obliczenia.js
+++ kalkulator-przyklad/js/obliczenia.js
@@ -1,4 +1,32 @@
-var liczba = document.getElementsByClassName("val");
 
-document.getElementById("wynik").innerHTML = liczba[1].innerHTML;
-console.log(liczba[1].innerHTML);
+//przypisuję każdemu przyciskowi event click i każę mu iść do funkcji pokaz
+var znaki = document.getElementsByClassName("a");
+var i;
+for (i=0; i<znaki.length; i++) {
+    'use strict';
+    znaki[i].addEventListener("click", pokaz);
+}
+
+function pokaz(){
+    
+    //budowanie działania
+    wynik.innerHTML += this.innerText;
+    
+    // akcja przycisku obliczającego
+    var rowna_sie = document.getElementById("rowna_sie");
+    rowna_sie.addEventListener("click", oblicz);
+    
+    //funckja obliczająca
+    function oblicz(){
+        wynik.innerHTML = eval(wynik.innerHTML);
+    }
+    
+    //akcja przycisku czyszczącego
+    var del = document.getElementById("del");
+    del.addEventListener("click", wyczysc);
+    
+    //funkcja czyszcząca
+    function wyczysc() {
+        wynik.innerHTML = "";
+    }
+}
